1f5acd9491a2a1a64eb493dc5b58996d282065a8
maria herrero sanchez <mariaherrerosanchezgg@gmail.com>
2026-07-30 08:48:40 +0200

    feat(frontend): add Learn page with educational resources

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -27,6 +27,9 @@ export default function Navbar() {
         <NavLink to="/rgpd" className="navbar__link">
           RGPD
         </NavLink>
+        <NavLink to="/aprende" className="navbar__link">
+          Aprende
+        </NavLink>
         <NavLink to="/modelo" className="navbar__link">
           Modelo
         </NavLink>
